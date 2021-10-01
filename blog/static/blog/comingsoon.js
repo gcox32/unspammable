755b0e2d192a2e16a4b60ff09daa8fd706810704
gcox32 <gcoxexcel@gmail.com>
2021-10-01 14:22:52 -0400

    tweaked marvel css

--- blog/static/blog/comingsoon.js
+++ blog/static/blog/comingsoon.js
@@ -29,3 +29,14 @@
       countdownSeconds.innerHTML = seconds;
     }, 1000);
   })();
+
+window.transitionToPage = function(href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      window.location.href = href
+  }, 500);
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1;
+});
