981867160d96a8855fa078c9ef187d2857177025
gcox32 <gcoxexcel@gmail.com>
2021-11-29 16:35:06 -0500

    templatized more views; added hamburger

--- finance/urls.py
+++ finance/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
+    path('', views.dashboard, name='finance'),
     path('giving', views.giving),
 ]
--- marvel/views.py
+++ marvel/views.py
@@ -5,14 +5,18 @@ from .src.timeline import timeline_list
 from .models import Platform, Credential
 
 
-def home(request):
+def get_platforms_credentials(request):
+    """helper function for loading in creds"""
     platforms = Platform.objects.values()
-
     try:
         credentials = Credential.objects.filter(user_id=request.user.id).values_list()
         credentials = list(credentials)
     except:
         credentials = [None]
+    return platforms, credentials
+
+def home(request):
+    platforms, credentials = get_platforms_credentials(request)
     
     context = {
         'platforms':platforms,
@@ -21,21 +25,22 @@ def home(request):
     return auth_check(request, 'marvelHome.html', context=context)
 
 def timeline(request):
-    return auth_check(request, 'timeline.html', context={'phases': timeline_list})
+    platforms, credentials = get_platforms_credentials(request)
+    context = {
+        'phases': timeline_list,
+        'platforms':platforms,
+        'credentials':credentials,
+    }
+    return auth_check(request, 'timeline.html', context=context)
 
 def phase(request, n):
     n = int(n)
     context = timeline_list[n]
     title_list = movies_by_phase(n)
-
-    try:
-        credentials = Credential.objects.filter(user_id=request.user.id).values_list()
-        credentials = list(credentials)
-    except:
-        credentials = [None]
+    platforms, credentials = get_platforms_credentials(request)
         
     context['titles'] = title_list
-    context['platforms'] = Platform.objects.values()
+    context['platforms'] = platforms
     context['credentials'] = credentials
 
     return auth_check(request, 'phase.html', context=context)
