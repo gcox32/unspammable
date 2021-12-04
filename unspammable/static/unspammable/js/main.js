18fab58ab0c2167b67ff4673d5a7c4f35b10ee73
gcox32 <gcoxexcel@gmail.com>
2021-12-04 09:55:39 -0500

    side bar js

--- unspammable/static/unspammable/js/main.js
+++ unspammable/static/unspammable/js/main.js
@@ -1,32 +1,32 @@
 // toggle off menu by clicking off menu
-window.onload = function get_body() {
+// window.onload = function get_body() {
 
-  body = document.getElementById('wrapper');
-  menu = document.getElementById('menuToggle');
+//   body = document.getElementById('wrapper');
+//   menu = document.getElementById('menu');
 
-};
+// };
 
-function attachMenuListener() {
-  body.addEventListener('click', toggleMenuOff);
-}
+// function attachMenuListener() {
+//   body.addEventListener('click', toggleMenuOff);
+// }
 
-function detachMenuListener() {
-  body.removeEventListener('click', toggleMenuOff);
-}
+// function detachMenuListener() {
+//   body.removeEventListener('click', toggleMenuOff);
+// }
 
-function toggleMenuOff() {
-  var currentState = menu.style.transform;
-  console.log(currentState);
-  if (currentState == "translate(-100%, 0)") {
-    menu.style.transform = "none";
-    attachMenuListener();
-    console.log('yep');
-  } else {
-    menu.style.transform = "translate(-100%, 0)";
-    detachMenuListener();
-    console.log('nope');
-  };
-}
+// function toggleMenuOff() {
+//   var currentState = menu.style.transform;
+//   console.log(currentState);
+//   if (currentState == "translate(-100%, 0)") {
+//     menu.style.transform = "none";
+//     attachMenuListener();
+//     console.log('yep');
+//   } else {
+//     menu.style.transform = "translate(-100%, 0)";
+//     detachMenuListener();
+//     console.log('nope');
+//   };
+// }
 
 
 
