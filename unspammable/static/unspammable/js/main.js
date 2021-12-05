c29f8bd12d20d307b6d177665be448b43524dc49
gcox32 <gcoxexcel@gmail.com>
2021-12-05 13:16:23 -0500

    cleaned up hamburger menu

--- unspammable/static/unspammable/js/main.js
+++ unspammable/static/unspammable/js/main.js
@@ -1,32 +1,29 @@
-// toggle off menu by clicking off menu
-// window.onload = function get_body() {
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const img = document.querySelector(".hamburger-image");
+const overlay = document.querySelector(".overlay");
 
-//   body = document.getElementById('wrapper');
-//   menu = document.getElementById('menu');
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    overlay.style.display = "none";
+    img.setAttribute("src", "/staticfiles/unspammable/images/hamburger.png");
+  } else {
+    menu.classList.add("showMenu");
+    overlay.style.display = "block";
+    img.setAttribute("src", "/staticfiles/unspammable/images/exit.png");
+  };
+};
 
-// };
-
-// function attachMenuListener() {
-//   body.addEventListener('click', toggleMenuOff);
-// }
-
-// function detachMenuListener() {
-//   body.removeEventListener('click', toggleMenuOff);
-// }
-
-// function toggleMenuOff() {
-//   var currentState = menu.style.transform;
-//   console.log(currentState);
-//   if (currentState == "translate(-100%, 0)") {
-//     menu.style.transform = "none";
-//     attachMenuListener();
-//     console.log('yep');
-//   } else {
-//     menu.style.transform = "translate(-100%, 0)";
-//     detachMenuListener();
-//     console.log('nope');
-//   };
-// }
+hamburger.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
 
+// retract menu before leaving page
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+);
 
