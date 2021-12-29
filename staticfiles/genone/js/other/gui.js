8cf343afc8039bc70b1aa953c073c7bb0600336f
gcox32 <gcoxexcel@gmail.com>
2021-12-29 13:00:02 -0500

    finished styling

--- staticfiles/genone/js/other/gui.js
+++ staticfiles/genone/js/other/gui.js
@@ -29,61 +29,25 @@ function windowingInitialize() {
 		console.log("Fatal windowing error: \"" + error.message + "\" file:" + error.fileName + " line: " + error.lineNumber, 2);
 	}
 	//Update the settings to the emulator's default:
-	document.getElementById("enable_sound").checked = settings[0];
+	document.getElementById("enable-sound").checked = settings[0];
 }
 function registerGUIEvents() {
 	console.log("In registerGUIEvents() : Registering GUI Events.", -1);
 	addEvent("click", document.getElementById("blue"), function() {
-		var vars = getVars(0);
-		var gameFile = vars[0]
-		var game = vars[1]
-		var clickedCode = vars[2]
-		var saveFileLoc = vars[3]
-
-		if (clickedCode == game + "-new") {
-			loadNewGameFunc(gameFile);
-		} else {
-			loadSavedGameFunc(saveFileLoc);
-		};
+		var [gameFile, game, clickedCode, saveFileLoc] = getVars(0);
+		loadSavedorNewGame(clickedCode, game, gameFile, saveFileLoc);
 	});
 	addEvent("click", document.getElementById("yellow"), function() {
-		var vars = getVars(1);
-		var gameFile = vars[0]
-		var game = vars[1]
-		var clickedCode = vars[2]
-		var saveFileLoc = vars[3]
-
-		if (clickedCode == game + "-new") {
-			loadNewGameFunc(gameFile);
-		} else {
-			loadSavedGameFunc(saveFileLoc);
-		};
+		var [gameFile, game, clickedCode, saveFileLoc] = getVars(1);
+		loadSavedorNewGame(clickedCode, game, gameFile, saveFileLoc);
 	});
 	addEvent("click", document.getElementById("red"), function() {
-		var vars = getVars(2);
-		var gameFile = vars[0]
-		var game = vars[1]
-		var clickedCode = vars[2]
-		var saveFileLoc = vars[3]
-
-		if (clickedCode == game + "-new") {
-			loadNewGameFunc(gameFile);
-		} else {
-			loadSavedGameFunc(saveFileLoc);
-		};
+		var [gameFile, game, clickedCode, saveFileLoc] = getVars(2);
+		loadSavedorNewGame(clickedCode, game, gameFile, saveFileLoc);
 	});
 	addEvent("click", document.getElementById("green"), function() {
-		var vars = getVars(3);
-		var gameFile = vars[0]
-		var game = vars[1]
-		var clickedCode = vars[2]
-		var saveFileLoc = vars[3]
-
-		if (clickedCode == game + "-new") {
-			loadNewGameFunc(gameFile);
-		} else {
-			loadSavedGameFunc(saveFileLoc);
-		};
+		var [gameFile, game, clickedCode, saveFileLoc] = getVars(3);
+		loadSavedorNewGame(clickedCode, game, gameFile, saveFileLoc);
 	});
 // 
 	addEvent("click", document.getElementById("saver"), function() {
@@ -148,19 +112,18 @@ function registerGUIEvents() {
 	});
 	addEvent("MozOrientation", window, GameBoyGyroSignalHandler);
 	addEvent("deviceorientation", window, GameBoyGyroSignalHandler);
-	addEvent("click", document.getElementById("enable_sound"), function () {
-		settings[0] = document.getElementById("enable_sound").checked;
+	addEvent("click", document.getElementById("enable-sound"), function () {
+		settings[0] = document.getElementById("enable-sound").checked;
 		if (GameBoyEmulatorInitialized()) {
 			gameboy.initSound();
 		}
 	});
-
 	addEvent("mouseup", document.getElementById("gfx"), initNewCanvasSize);
 	addEvent("resize", window, initNewCanvasSize);
 	addEvent("unload", window, function () {
 		autoSave();
 	});
-}
+};
 
 function changeVolume() {
 	if (GameBoyEmulatorInitialized()) {
@@ -171,7 +134,6 @@ function changeVolume() {
 		}
 	}
 };
-
 function keyDown(event) {
 	var keyCode = event.keyCode;
 	var keyMapLength = keyZones.length;
@@ -189,7 +151,7 @@ function keyDown(event) {
 			}
 		}
 	}
-}
+};
 function keyUp(event) {
 	var keyCode = event.keyCode;
 	var keyMapLength = keyZones.length;
@@ -207,11 +169,11 @@ function keyUp(event) {
 			}
 		}
 	}
-}
+};
 function initPlayer() {
 	document.getElementById("logo").style.display = "none";
 	document.getElementById("fullscreenContainer").style.display = "none";
-}
+};
 function fullscreenPlayer() {
 	if (GameBoyEmulatorInitialized()) {
 		if (!inFullscreen) {
@@ -231,8 +193,7 @@ function fullscreenPlayer() {
 	else {
 		console.log("Cannot go into fullscreen mode.", 2);
 	}
-}
-
+};
 //Some wrappers and extensions for non-DOM3 browsers:
 function isDescendantOf(ParentElement, toCheck) {
 	if (!ParentElement || !toCheck) {
@@ -256,33 +217,33 @@ function isDescendantOf(ParentElement, toCheck) {
 		return false;
 	}
 	return traverseTree(ParentElement.firstChild);
-}
+};
 function hasChildNodes(oElement) {
 	return (typeof oElement.hasChildNodes == "function") ? oElement.hasChildNodes() : ((oElement.firstChild != null) ? true : false);
-}
+};
 function isSameNode(oCheck1, oCheck2) {
 	return (typeof oCheck1.isSameNode == "function") ? oCheck1.isSameNode(oCheck2) : (oCheck1 === oCheck2);
-}
+};
 function pageXCoord(event) {
 	if (typeof event.pageX == "undefined") {
 		return event.clientX + document.documentElement.scrollLeft;
 	}
 	return event.pageX;
-}
+};
 function pageYCoord(event) {
 	if (typeof event.pageY == "undefined") {
 		return event.clientY + document.documentElement.scrollTop;
 	}
 	return event.pageY;
-}
+};
 function mouseLeaveVerify(oElement, event) {
 	//Hook target element with onmouseout and use this function to verify onmouseleave.
 	return isDescendantOf(oElement, (typeof event.target != "undefined") ? event.target : event.srcElement) && !isDescendantOf(oElement, (typeof event.relatedTarget != "undefined") ? event.relatedTarget : event.toElement);
-}
+};
 function mouseEnterVerify(oElement, event) {
 	//Hook target element with onmouseover and use this function to verify onmouseenter.
 	return !isDescendantOf(oElement, (typeof event.target != "undefined") ? event.target : event.srcElement) && isDescendantOf(oElement, (typeof event.relatedTarget != "undefined") ? event.relatedTarget : event.fromElement);
-}
+};
 function addEvent(sEvent, oElement, fListener) {
 	try {	
 		oElement.addEventListener(sEvent, fListener, false);
@@ -292,7 +253,7 @@ function addEvent(sEvent, oElement, fListener) {
 		oElement.attachEvent("on" + sEvent, fListener);	//Pity for IE.
 		// console.log("In addEvent() : Nonstandard attachEvent() called to add an \"on" + sEvent + "\" event.", -1);
 	}
-}
+};
 function removeEvent(sEvent, oElement, fListener) {
 	try {	
 		oElement.removeEventListener(sEvent, fListener, false);
@@ -302,8 +263,7 @@ function removeEvent(sEvent, oElement, fListener) {
 		oElement.detachEvent("on" + sEvent, fListener);	//Pity for IE.
 		// console.log("In removeEvent() : Nonstandard detachEvent() called to remove an \"on" + sEvent + "\" event.", -1);
 	}
-}
-
+};
 
 // new stuff
 function loadNewGame(filepath, callback) {
@@ -324,7 +284,6 @@ function loadNewGame(filepath, callback) {
 	};
 	return [myBlob, myFile]
 };
-
 function uploadSaveFile(file, savename) {
 	str = JSON.stringify(file)
 	const filename = savename + ".json";
@@ -339,8 +298,7 @@ function uploadSaveFile(file, savename) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", dest, true);
 	xhr.send(formData);
-}
-
+};
 function loadSavedGame(filepath, callback) {
 	var xhr = new XMLHttpRequest();
 	const filename = filepath.split('/')[2];
@@ -355,7 +313,6 @@ function loadSavedGame(filepath, callback) {
 	};
 	return saveStateArray;
 };
-
 function getVars(gameIdx) {
 	var gameFile = gameList[gameIdx];
 	var game = gameFile.split('.')[0];
@@ -366,8 +323,7 @@ function getVars(gameIdx) {
 	var clickedCode = clicked.getAttribute('value');
 	var saveFileLoc = clicked.getAttribute('name');
 	return [gameFile, game, clickedCode, saveFileLoc]
-}
-
+};
 function loadNewGameFunc(gameFile) {
 	var cartridge = loadNewGame('/genone/roms/' + gameFile, function(blob, file) {
 		var reader = new FileReader();
@@ -377,7 +333,7 @@ function loadNewGameFunc(gameFile) {
 		});
 		reader.readAsBinaryString(file);
 	});
-}
+};
 function loadSavedGameFunc(saveFileLoc) {
 	var savedGame = loadSavedGame('/genone/roms/savestates/' + saveFileLoc, function(saveState){
 		clearLastEmulation();
@@ -386,4 +342,16 @@ function loadSavedGameFunc(saveFileLoc) {
 		gameboy.returnFromState(saveState);
 		run();
 	});
-}
+};
+function loadSavedorNewGame(clickedCode, game, gameFile, saveFileLoc) {
+	backgroundSwitch(game);
+	if (clickedCode == game + "-new") {
+		loadNewGameFunc(gameFile);
+	} else {
+		loadSavedGameFunc(saveFileLoc);
+	};
+};
+function backgroundSwitch(version) {
+	var bg = document.getElementsByTagName('body')[0];
+	bg.style.backgroundImage = 'url(\"/staticfiles/genone/images/bg-' + version + '.png\")'
+};
