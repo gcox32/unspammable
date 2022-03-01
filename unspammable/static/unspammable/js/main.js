5bf69e1a90373ce23eb8358fece3e42cbe67e366
gcox32 <gcoxexcel@gmail.com>
2022-03-01 14:23:34 -0500

    cleaned up js

--- unspammable/static/unspammable/js/main.js
+++ unspammable/static/unspammable/js/main.js
@@ -8,11 +8,9 @@ function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
     overlay.style.display = "none";
-    // img.setAttribute("src", "/staticfiles/unspammable/images/icons/clear_bg_3_white.png");
   } else {
     menu.classList.add("showMenu");
     overlay.style.display = "block";
-    // img.setAttribute("src", "/staticfiles/unspammable/images/icons/clear_bg_3_white.png");
   };
 };
 
@@ -27,3 +25,48 @@ submenuItems.forEach(
   }
 );
 
+var btn = document.getElementById('modal_opener');
+var modal = document.querySelector('.modal');
+
+function attachModalListeners(modalElm) {
+  modalElm.querySelector('.close_modal').addEventListener('click', toggleModal);
+  modalElm.querySelector('.modal_overlay').addEventListener('click', toggleModal);
+}
+
+function detachModalListeners(modalElm) {
+  modalElm.querySelector('.close_modal').removeEventListener('click', toggleModal);
+  modalElm.querySelector('.modal_overlay').removeEventListener('click', toggleModal);
+}
+
+function toggleModal() {
+  var currentState = modal.style.display;
+
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    overlay.style.display = "none";
+  }
+  // If modal is visible, hide it. Else, display it.
+  if (currentState === 'none') {
+    modal.style.display = 'block';
+    attachModalListeners(modal);
+  } else {
+    modal.style.display = 'none';
+    detachModalListeners(modal);  
+  }
+}
+
+function copyToClipboard(id, n) {
+  /* Get the text field */
+  var copyText = document.getElementById(id);
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.innerHTML);
+
+  var tooltip = document.getElementsByClassName("tooltiptext")[n];
+  tooltip.innerHTML = "Copied";
+}
+
+function outFunc(n) {
+  var tooltip = document.getElementsByClassName("tooltiptext")[n];
+  tooltip.innerHTML = "Copy";
+}
