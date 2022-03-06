707f628bc0bab854ddd92799ceae02b64262fdf5
gcox32 <gcoxexcel@gmail.com>
2022-03-06 13:58:17 -0500

    wtp bugfix 0.3

--- genone/static/genone/js/guess/pokemon.js
+++ genone/static/genone/js/guess/pokemon.js
@@ -441,12 +441,16 @@ function generateNewNumbers(force) {
         upcomingPokemonArrayPos = 0;
         console.log(allGenerations);
         newGen.filter(function(gen) { 
-            console.log(gen);
             return allGenerations[gen].supportedDifficulties.includes(settings.difficulty); })
             .forEach(function(genToInc) {
-            (_.range(allGenerations[genToInc].start, allGenerations[genToInc].end + 1)).forEach(function (pokemonNumber) {
-                upcomingPokemon.push(pokemonNumber);
-            });
+                console.log(genToInc);
+                try {
+                    (_.range(allGenerations[genToInc].start, allGenerations[genToInc].end + 1)).forEach(function (pokemonNumber) {
+                        upcomingPokemon.push(pokemonNumber);
+                    });
+                } catch(err) {
+                    console.log(err);
+                }
         });
 
         upcomingPokemon = _.shuffle(upcomingPokemon);
