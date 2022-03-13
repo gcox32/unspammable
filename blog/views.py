cbad62275cf694b5c70ad92bc1610e263e7e5435
gcox32 <gcoxexcel@gmail.com>
2022-03-12 20:59:02 -0500

    initial comments commit

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,33 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blogIndex.html'
 
-def post_detail(request, slug):
-    context = {}
-    context['blog'] = Post.objects.get(slug=slug)
-    
-    return render(request, 'postDetail.html', context)
-
 def list_by_tag(request, slug): 
     context = {}
     tag = Tag.objects.get(slug=slug).name
     context['blog_posts'] = Post.objects.filter(tags__name=tag)
 
     return render(request, 'blogIndex.html', context)
+
+from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, slug):
+    template_name = 'postDetail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
