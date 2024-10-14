d45a885b18d404b3ce3502dd405fdee952d92590
Grant Cox <42614867+gcox32@users.noreply.github.com>
2024-10-14 12:24:31 -0400

    Finish adding basic auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,27 @@
 
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
+// const client = generateClient<Schema>();
 
-const client = generateClient<Schema>();
-
-export default function App() {
-
+export default function HomePage() {
+  const { signOut } = useAuthenticator();
+  
   return (
     <main>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Proceed.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
